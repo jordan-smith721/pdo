2c1d3c0e4e2e4a9c6c85edfc50ecda99b13b2573
Jordan Smith <jordan.m.smith721@gmail.com>
2019-02-11 18:03:34 -0500

    defined an add statement

--- index.php
+++ index.php
@@ -6,14 +6,36 @@
  * Time: 2:10 PM
  */
 
+//error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 //Connect to DB
-require '/home/jsmithgr_grcuser/config.php';
+require '/home/jsmithgr/config.php';
 try {
     //Instantiate a db object
-    $dbh= new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
+    $dbh = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
     echo "Connected to DB!";
 }
 catch(Exception $ex)
 {
     echo $ex->getMessage();
 }
+
+//define the query
+$sql = "INSERT INTO pets(type, name, color)
+          VALUES (:type, :name, :color)";
+
+//prepare the statement
+$statement = $dbh->prepare($sql);
+
+//bind the parameters
+$type = 'kangaroo';
+$name = 'Joey';
+$color = 'purple';
+$statement->bindParam(':type', $type, PDO::PARAM_STR);
+$statement->bindParam(':name', $name, PDO::PARAM_STR);
+$statement->bindParam(':color', $color, PDO::PARAM_STR);
+
+//Execute
+$statement->execute();
